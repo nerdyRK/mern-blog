1b42ed371fe8c6295a0d9653b0f466b10e62b6c4
Ravi Kumar <rav7179@gmail.com>
2024-06-19 16:34:16 +0530

    added database connection and DB in atlas

---
+++ app.js
@@ -0,0 +1,32 @@
+import 'express-async-errors';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Database Connection
+connectDB();
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
